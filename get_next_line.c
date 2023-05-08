c113844c71a0200170cbf7ab37b3d38afe4d46df
cwenz <cwenz@student.42heilbronn.de>
2023-05-08 09:37:53 +0200

    added get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,21 @@
 /*   By: cwenz <cwenz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 10:48:29 by cwenz             #+#    #+#             */
-/*   Updated: 2023/05/05 13:34:30 by cwenz            ###   ########.fr       */
+/*   Updated: 2023/05/08 09:37:08 by cwenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 /**
-** @brief Extracts one line at a time from a 
-**	text file the file descriptor is pointing to
-** @param fd The file descriptor to read from
-** @return
+ * @brief Extracts one line at a time from a text file
+ *  the file descriptor is pointing to and allocates
+ *  dynamically, which must be freed by the caller. Stops reading 
+ *  at the first newline character or EOF (End of file).
+ * @param fd The file descriptor to read from
+ * @return A pointer to the newly allocated null-terminated string 
+ *  containing the newline, or NULL if the input fd fails, BUFFER_SIZE 
+ *  is less than 1, the EOF has been reached.
 */
 char	*get_next_line(int fd)
 {
@@ -29,15 +33,27 @@ char	*get_next_line(int fd)
 	if (!buffer)
 		return (NULL);
 	line = set_current_line(buffer);
+	// Check if line variable (NULL or '\0') is an empty string and
+	// if there is no newline in the buffer variable
+	// this means the file is empty or the file has reached the end
 	if (line[0] == '\0' && !ft_strchr(buffer, '\n')) {
 		free(line);
 		buffer = free_upto(buffer);
 		return (NULL);
 	}
-	buffer = free_upto(buffer); // Free up static char
+	buffer = free_upto(buffer);
 	return (line);
 }
 
+/**
+ * @brief Reads from a file descriptor until a newline character is found or if 
+ *  end of file has been reached. It reads in chunks of BUFFER_SIZE and appends
+ *  the content to the input buffer variable.
+ * @param buffer The null-terminated input buffer to append content to.
+ * @param fd The file descriptor to read from.
+ * @return A pointer to the updated input buffer containing the newly appended contetn,
+ *  or NULL if read() fails or if the end of file has been reached.
+ */
 char	*get_current_line(char *buffer, int fd)
 {
 	char	*temp;
@@ -65,7 +81,14 @@ char	*get_current_line(char *buffer, int fd)
 	free(temp);
 	return (buffer);
 }
-
+/**
+ * @brief Extracts the current line from a null terminated string up to and
+ *  including the newline character and assigns to a variable. The strings memory is dynamically
+ *  allocated, but must be freed by the caller to avoid memory leaks.
+ *  @param str The null terminated string to extract a line from.
+ * @return A pointer to the newly created string containing all characters up to and including the newline
+ *  or NULL on allocation or if the input string (str) is NULL.
+*/
 char	*set_current_line(char *str)
 {
 	int		i;
@@ -75,7 +98,7 @@ char	*set_current_line(char *str)
 	line = NULL;
 	if (!str)
 		return (NULL);
-	while (str[i] != '\0' && str[i] != '\n')
+	while (str[i] && str[i] != '\n')
 		i++;
 	if (str[i] == '\n')
 		i++;
@@ -89,12 +112,22 @@ char	*set_current_line(char *str)
 		i++;
 	}
 	line[i] = str[i];
-	if (line[i] != '\0')
+	if (line[i])
 		line[++i] = '\0';
 	return (line);
 }
 
-
+/**
+ * @brief Frees memory allocated for a string up to the first newline character
+ *	and returns the remaining part of the string.
+ *  This function takes a null terminated string and searches for the first newline character
+ *	and creates a new string with the remaining characters after the newline. The original memory
+ *  is freed. The new string's memory is dynamically calculated, but must be freed by the caller after
+ *  use to avoid memory leaks.
+ * @param str The null-terminated string to check.
+ * @return A pointer to the newly allocated string containing everything after the first found newline 
+ *  character, or NULL on allocation or if no newline is found.
+*/
 char	*free_upto(char *str)
 {
 	char	*temp;
@@ -112,6 +145,7 @@ char	*free_upto(char *str)
 		return (free(str), NULL);
 	i++;
 	// Since we found a newline, assign rest of chars to temp variable
+	// Allocate memory for everything after newline
 	temp = malloc(sizeof(char) * ft_strlen(str) - i + 1);
 	if (!temp)
 		return (free(str), NULL);
@@ -119,7 +153,7 @@ char	*free_upto(char *str)
 		temp[j++] = str[i++];
 	temp[j] = '\0';
 	free(str);
-	return (temp); // Return the temp value (everything after newline)
+	return (temp);
 }
 
 // int	main(void)
